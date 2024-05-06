292ad9d88e86566559dfe73be4932fbbdcaee6b8
DeathFull <contact@deathfull.net>
2024-05-06 16:21:24 +0200

    update: rework if condition

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,46 +1,50 @@
 import HeaderLogin from "./HeaderLogin.tsx";
 import Header from "./Header.tsx";
 import Footer from "./Footer.tsx";
-import {Box, Flex} from "@chakra-ui/react";
-import {Outlet, useLocation} from "react-router-dom";
+import { Box, Flex } from "@chakra-ui/react";
+import { Outlet, useLocation } from "react-router-dom";
+import DashboardHeader from "./dashboard/DashboardHeader.tsx";
 
 function MainPage() {
-    const location = useLocation();
+  const location = useLocation();
 
-    if (location.pathname === "/login") {
-        return (
-            <>
-                <Flex pos="relative" direction="column" minH="100vh">
-                    <HeaderLogin/>
-                    <Flex justify={"center"} direction={"column"} flex={1}>
-                        <Outlet/>
-                    </Flex>
-                    <Footer/>
-                </Flex>
-            </>
-        );
-    } else {
-        return (
-            <>
-                <Box
-                    w="100vw"
-                    h="100vh"
-                    bgImage="url('/NavBar.png')"
-                    bgSize="contain"
-                    bgPosition="top right"
-                    bgRepeat="no-repeat"
-                >
-                    <Flex pos="relative" direction="column" minH="100vh">
-                        <Header/>
-                        <Flex justify={"center"} direction={"column"} flex={1}>
-                            <Outlet/>
-                        </Flex>
-                        <Footer/>
-                    </Flex>
-                </Box>
-            </>
-        );
-    }
+  if (location.pathname === "/" || location.pathname === "/about") {
+    return (
+      <>
+        <Box
+          w="100vw"
+          h="100vh"
+          bgImage="url('/NavBar.png')"
+          bgSize="contain"
+          bgPosition="top right"
+          bgRepeat="no-repeat"
+        >
+          <Flex pos="relative" direction="column" minH="100vh">
+            <Header />
+            <Flex justify={"center"} direction={"column"} flex={1}>
+              <Outlet />
+            </Flex>
+            <Footer />
+          </Flex>
+        </Box>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Flex pos="relative" direction="column" minH="100vh">
+          {location.pathname.startsWith("/dashboard") ? (
+            <DashboardHeader />
+          ) : (
+            <HeaderLogin />
+          )}
+          <Flex flex={1} h="100%">
+            <Outlet />
+          </Flex>
+        </Flex>
+      </>
+    );
+  }
 }
 
 export default MainPage;
